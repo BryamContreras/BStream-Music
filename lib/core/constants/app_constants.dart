398b1b6940c9bd1be9c9c1ddb48088fa692600ab
Bryam Contreras <briamjoseelias@gmail.com>
2026-08-04 15:09:14 -0600

    Actualizar información de la versión v1.2.2

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -2,7 +2,7 @@ class AppConstants {
   const AppConstants._();
 
   static const appName = 'BStream Music';
-  static const appVersion = '1.2.1';
+  static const appVersion = '1.2.2';
   static const databaseName = 'bstream_music.db';
   static const databaseVersion = 3;
   static const androidYtdlChannel = 'bstream_music/ytdl';
